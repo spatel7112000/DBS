b7b7f3dfd4a76ebe1a5dc8e7ce69c5bd99951868
sAcHiN pAtEl <67690850+spatel7112000@users.noreply.github.com>
2022-09-11 21:27:16 +0530

    Update upload.js

--- upload/upload.js
+++ upload/upload.js
@@ -1,25 +1,26 @@
 const express = require('express')
 const app = express()
-const multer = require('multer')
-const path = require('path')
-
-const upload = multer({
-    storage:multer.diskStorage({
-    destination: './upload/images',
-    limits:{fileSize:100},
-    filename: (req, file, cb)=>{
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-})
+const ConnectDB = require('./DBconnect/config')
+ConnectDB();
+const upload = require('./middleware/upload')
 
 app.use('/profile', express.static('upload/images'))
-app.post('/upload', upload.single('profile'), (req, res)=>{
+
+app.post('/single', upload.single('profile'), (req, res)=>{
     console.log(req.file);
     res.json({
         success:1,
-        profile_url: `http://localhost:4000/profile/${req.file.filename}`
+        profile_url: `http://localhost:4000/${req.file.filename}`
     })
+})
+app.post('/multiple', upload.array('profile', 4), (req, res)=>{
+    console.log(req.file);
+    try{
+        res.send(req.files);
+    } catch(error){
+        console.log(error);
+        res.send(400);
+    }
 })
 /* function errHandler(err, req, res, next){
     if(err instanceof multer.MulterError){
@@ -31,6 +32,6 @@ app.post('/upload', upload.single('profile'), (req, res)=>{
 }
 app.use(errHandler);
 */
-app.listen(4000, ()=>{
+app.listen(5000, ()=>{
     console.log('server is running');
 })
